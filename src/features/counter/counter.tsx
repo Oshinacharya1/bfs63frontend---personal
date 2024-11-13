da384b10309a38d77bc87d0f5b33d636b1def213
Oshin Acharya <oshin.acharya0664@gmail.com>
2024-11-13 09:17:01 -0600

    added css

--- src/features/counter/counter.tsx
+++ src/features/counter/counter.tsx
@@ -9,13 +9,15 @@ export function Counter() {
     <div>
       <div>
         <button
+          className="bg-yellow-400 p-2 m-2 rounded"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Increment
         </button>
-        <span>{count}</span>
+        <span className="rounded-full bg-purple-300 p-2">{count}</span>
         <button
+          className="bg-yellow-400 p-2 m-2 rounded"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
